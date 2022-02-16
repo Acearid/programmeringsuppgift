0b7e902e11b85ff01453a892631ab9a81f6a3842
Acearid <noel.dansell@gmail.com>
2022-02-16 17:05:51 +0100

    added code

--- Noel/Noel.py
+++ Noel/Noel.py
@@ -1,1 +1,26 @@
-print("hello world!")
+#importerade moduler
+import math
+import numpy as np
+import statistics as st
+
+#funktionerna
+def medel(x):
+    return sum(x) / len(x)
+def standardav(x):
+    return np.std(x)
+def medianen(x):
+    return st.median(x)
+
+#listan
+x = [1,2,3,4,5]
+
+#variablerna för svaren
+svarmed = medel(x)
+svarstandard = standardav(x)
+svarmedian = medianen(x)
+
+#skriver ut svaren
+print("medelvärde: ", svarmed)
+print("standard avikelse: ", svarstandard )
+print("median: ", svarmedian)
+
