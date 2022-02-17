85a0d63fdc2c3df551f6b1293b03193461181592
Acearid <noel.dansell@gmail.com>
2022-02-17 15:09:51 +0100

    lets goo

--- Noel/kvartil.py
+++ Noel/kvartil.py
@@ -1,18 +1,18 @@
 import statistics as st
-
+from sammling import median
 def kvartilun(ls):
     ls.sort()
     return ls[0:int(len(ls)/2)]
 def kvartilup(ls):
     ls.sort()
-    return ls[int(st.median(ls)):-1]
-def kvartilavst():
-    up = st.median(kvartilup(ls))
-    un = st.median(kvartilun(ls))
+    return ls[int(median(ls)):-1]
+def kvartilavst(ls):
+    up = median(kvartilup(ls))
+    un = median(kvartilun(ls))
     return up - un
 
 ls = [1,2,11,3,14,13,5,6,7,22]
 
-print("övre kvartil: ",st.median(kvartilup(ls)))
-print("nedre kvartil: ", st.median(kvartilun(ls)))
-print("kvartil avstånd: ", kvartilavst())
+print("övre kvartil: ",median(kvartilup(ls)))
+print("nedre kvartil: ", median(kvartilun(ls)))
+print("kvartil avstånd: ", kvartilavst(ls))
---
+++ Noel/sammling.py
@@ -0,0 +1,26 @@
+def median(ls):
+    n = len(ls)
+    x = n // 2
+    if n % 2:
+        return sorted(ls)[x]
+    return sum(sorted(ls)[x - 1:x + 1]) / 2
+def std_dev(ls):
+    n = len(ls)
+    mean = sum(ls) / n
+    var = sum((x - mean)** 2 for x in ls) / n
+    std_dev = var ** 0.5
+    return std_dev
+def medel(x):
+    return sum(x) / len(x)
+def varbred(x):
+    return max(x) - min(x)
+def kvartilun(ls):
+    ls.sort()
+    return ls[0:int(len(ls)/2)]
+def kvartilup(ls):
+    ls.sort()
+    return ls[int(median(ls)):-1]
+def kvartilavst(ls):
+    up = median(kvartilup(ls))
+    un = median(kvartilun(ls))
+    return up - un
